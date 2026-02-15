d622e79645f3e155a345b4e47faa42de648b1ba3
Loic <loic.andrianarivony@ynov.com>
2026-02-15 20:29:16 +0100

    Mise à jour des classes de paiement

--- src/ecommerce/payment/CreditCardPayment.java
+++ src/ecommerce/payment/CreditCardPayment.java
@@ -2,5 +2,7 @@ public class CreditCardPayment implements PaymentMethod {
     @Override public void pay(double amount) {
         System.out.println("✅ Paiement CB effectué : " + amount + " €");
     }
-    @Override public String name() { return "CB"; }
+    @Override public String name() { 
+        return "CB"; 
+    }
 }
