ffd0efdf61dce3ee21761e53ed9bdc3071fcf9eb
Loic <loic.andrianarivony@ynov.com>
2026-02-15 21:06:43 +0100

    Ajout de StandardShipping

---
+++ src/ecommerce/shipping/StandardShipping.java
@@ -0,0 +1,11 @@
+package ecommerce.shipping;
+
+import ecommerce.model.Order;
+
+public class StandardShipping implements ShippingMethod {
+    @Override public void ship(Order order) {
+        System.out.println("📦 Expédition STANDARD de la commande " + order.getId());
+    }
+    @Override public double fee() { return 4.99; }
+    @Override public String name() { return "Standard"; }
+}
