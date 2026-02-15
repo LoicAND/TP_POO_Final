5a374d25c8c47c60d2288952ef3b657b7cf26fcb
Loic <loic.andrianarivony@ynov.com>
2026-02-15 20:27:01 +0100

    Ajout de PaypalPayment

---
+++ src/ecommerce/payment/PaypalPayment.java
@@ -0,0 +1,7 @@
+public class PaypalPayment implements PaymentMethod {
+    @Override public void pay(double amount) {
+        System.out.println("✅ Paiement PayPal effectué : " + amount + " €");
+    }
+    @Override public String name() { return "PayPal"; }
+}
+
