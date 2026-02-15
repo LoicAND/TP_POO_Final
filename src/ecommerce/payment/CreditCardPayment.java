3c678e7c976df8b67e609288ba0731cdd9f9b664
Loic <loic.andrianarivony@ynov.com>
2026-02-15 20:26:14 +0100

    Ajout de CreditCardPayment

---
+++ src/ecommerce/payment/CreditCardPayment.java
@@ -0,0 +1,6 @@
+public class CreditCardPayment implements PaymentMethod {
+    @Override public void pay(double amount) {
+        System.out.println("✅ Paiement CB effectué : " + amount + " €");
+    }
+    @Override public String name() { return "CB"; }
+}
